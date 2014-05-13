5991d2b5a79c202a13f9254429847cf6607a462a
Jose Luis Fonseca <desarrollo@reddesignsystems.com>
2014-05-13 11:39:23 -0500

    Fixing a override when there is more than one field in a single form, they all got the value from the last field.

--- views/input.php
+++ views/input.php
@@ -1,16 +1,16 @@
 <div class="input-append bootstrap-timepicker">
-    <input name="<?php  echo $nameform ?>" type="text" class="timepicker" readonly="readonly" value="<?php echo $value ?>">
+    <input name="<?php echo $nameform ?>" type="text" class="timepicker" readonly="readonly" value="<?php echo $value ?>">
     <span class="add-on"><i class="icon-time"></i></span>
 </div>
 <script>
     $(function(){
-        $('.timepicker').timepicker();
+        $('[name="<?php echo $nameform ?>"]').timepicker();
     })
 </script>
 <?php if(!empty($value)): ?>
 <script>
     $(function(){
-        $('.timepicker').timepicker('setTime', '<?php echo $value ?>');
+        $('[name="<?php echo $nameform ?>"]').timepicker('setTime', '<?php echo $value ?>');
     })
 </script>
 <?php endif; ?>
