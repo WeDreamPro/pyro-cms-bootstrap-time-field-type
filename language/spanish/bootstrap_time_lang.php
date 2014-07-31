0e26f4f1bf6299aecdb1b7015339b7ad88fd18f8
Jose Luis Fonseca <desarrollo@reddesignsystems.com>
2014-07-31 11:14:50 -0500

    Change the name for spanish

--- language/spanish/bootstrap_time_lang.php
+++ language/spanish/bootstrap_time_lang.php
@@ -1,6 +1,6 @@
 <?php
 
 
-$lang['streams:bootstrap_time.name'] = 'Bootstrap Time Picker';
+$lang['streams:bootstrap_time.name'] = 'Seleccionador de Hora';
 $lang['streams.format_time.instructions'] = 'Por favor ingrese el formato de hora para mostrar una vez guardado el dato, use formatos de hora php, por defecto usara g:i A';
 $lang['streams:bootstrap_time.format'] = 'Formato de hora';
